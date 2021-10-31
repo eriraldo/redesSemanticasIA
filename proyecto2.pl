2c41b3aaabbd601f10ee32244f86253129fe3016
Steven Retana <stevenretana@estudiantec.cr>
2021-10-31 00:17:36 -0600

    Pregunta 2 agregada

--- proyecto2.pl
+++ proyecto2.pl
@@ -1,4 +1,20 @@
-%Hechos
+/*
+------------------------------------------------------
+|                                                    | 
+|        PROYECTO # 2: REDES SEMÁNTICAS              |
+|        CURSO: INTELIGENCIA ARTIFICIAL              |
+|        PROFESOR: JORGE VARGAS CALVO                |
+|                                                    |
+|        INTEGRANTES:                                |
+|           STEVEN RETANA CEDEÑO - 2017144537        |    
+|           ERICK ELIZONDO       - 20........        |
+|           ERICK SALAZAR        - 20........        |
+|           DAVID ESPINOZA       - 20........        |
+------------------------------------------------------
+*/
+
+/* Conceptos acerca de los productos financieros en Costa Rica */
+
 es_Un(persona, cliente).
 es_Un(empresa, cliente).
 es_Un(intitucionesPublicas, cliente).
@@ -8,7 +24,7 @@ es_Un(productoFinanciero, instrumentoFinanciero).
 es_Un(dinero,inversion).
 es_Un(codigo_de_comercio, legislacion).
 es_Un(bancoCostaRica, bancoPublico).
-es_Un(bancoPopular, bancoPublico).
+
 es_Un(titulo_valores, instrumentoFinanciero).
 es_Un(accion, participacion_en_propiedad_de_la_compania).
 es_Un(accion,instrumento_con_pago_de_dividendos).
@@ -37,6 +53,9 @@ regula(legislacion,titulo_valores).
 
 dura_Un(lapso_de_tiempo,plazo).
 
+
+/* Hechos */
+
 emite(bancoPublico,certificado_de_deposito_a_plazo).
 emite(la_nacion,certificado_de_deposito_a_plazo).
 emite(holcim,certificado_de_deposito_a_plazo).
@@ -49,6 +68,14 @@ emite(holcim,accion).
 emite(fifco,accion).
 emite(ministerio_de_hacienda, certificado_de_deuda_publica).
 
-%Reglas
+/* Reglas */
+
 es_un_instrumentoFinanciero(A) :- es_Un(A, instrumentoFinanciero).
 es_un_instrumentoFinanciero(A) :- es_Un(A, B), es_Un(B, instrumentoFinanciero).
+
+/* Pregunta 2: "Una empresa compra acciones de FIFCO ¿recibirá acciones?" */
+
+recibe_intereses(Empresa) :- 
+    emite(Empresa, accion),
+    es_Un(accion, instrumento_con_pago_de_intereses).
+
